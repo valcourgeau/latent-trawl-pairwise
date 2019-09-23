d4e9184b668917eb50d4fff54f7a4435384213ca
Valentin Courgeau <valentin.courgeau@free.fr>
2019-09-23 11:47:11 +0100

    add exponential trawl sep file

---
+++ trawl_exponential.R
@@ -0,0 +1,19 @@
+ExponentialTrawl <- new.env()
+
+ExponentialTrawl$TrawlB1 <- function(param, h){
+  stopifnot(length(param) == 1)
+  # cat('param B1', param, 'h', h, '\n')
+  # assertthat::assert_that(CheckAllPositive(c(param, h)), msg = cat('params', param, '/ h', h,'\n'))
+  return((1.0- exp(-param*h))/param)
+}
+
+ExponentialTrawl$TrawlB2 <- function(param, h){
+  stopifnot(length(param) == 1)
+  # cat('param B2', param, 'h', h, '\n')
+  # assertthat::assert_that(CheckAllPositive(c(param, h)), msg = cat('params', param, '/ h', h,'\n'))
+  return(exp(-param*h)/param)
+}
+
+ExponentialTrawl$TrawlB3 <- function(param, h){
+  return(TrawlExpB1(param, h))
+}
