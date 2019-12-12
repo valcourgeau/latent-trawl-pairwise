3fcb98d26d34d7f8cdb3908ae74b495fce840e88
Valentin Courgeau <valentin.courgeau@free.fr>
2019-12-12 11:25:41 +0000

    fix conditional simulation and added uniform rescaling

--- utils_vine_data.R
+++ utils_vine_data.R
@@ -42,29 +42,49 @@ FilterExtremeIndex <- function(dataset, col_number, horizon){
   return(index_pick)
 }
 
-ExtremeVineData <- function(dataset, uniform_dataset, col_number, horizon){
+ExtremeVineData <- function(dataset, uniform_dataset, col_number, horizon, rescaling=F){
+  assertthat::are_equal(nrow(dataset), nrow(uniform_dataset))
+  assertthat::are_equal(ncol(dataset), ncol(uniform_dataset))
+  
   index_pick <- FilterExtremeIndex(dataset, col_number, horizon)
   xvine_data <- vapply(
     1:ncol(dataset),
     function(i){uniform_dataset[index_pick[,i],i]},
     index_pick[,1])
-  return(xvine_data)
+  
+  if(rescaling){
+    xvine_data <- apply(xvine_data, MARGIN = 2,
+                        function(x){ecdf_tmp <- ecdf(x); return(ecdf_tmp(x))})
+  }
+  
+  extreme_data <- vapply(
+    1:ncol(dataset),
+    function(i){dataset[index_pick[,i],i]},
+    index_pick[,1])
+  xvne_proba_zero <- 1-apply(extreme_data > 0, 2, mean)
+  quantiles <- xvne_proba_zero
+  quantile_values <- vapply(1:ncol(uniform_dataset),
+                            function(i){quantile(xvine_data[,i], quantiles[i])}, 1.0)
+  return(list(xvine_data=xvine_data, quantiles=quantiles, quantile_values=quantile_values))
 }
 
-ExtremeVineFit <- function(dataset, uniform_dataset, col_number, horizon, vine_config){
-  xvine_data <- ExtremeVineData(dataset, uniform_dataset, col_number, horizon)
+ExtremeVineFit <- function(dataset, uniform_dataset, col_number, horizon, vine_config, rescaling=F){
+  xvine_data <- ExtremeVineData(dataset, uniform_dataset, col_number, horizon, rescaling)
+  
   time_before <- Sys.time()
+  cat('Starting fit on column', col_number, '\n')
   vine_fit <- rvinecopulib::vinecop(
-    data = xvine_data,
+    data = xvine_data$xvine_data,
     family_set = vine_config[['family_set']],
     trunc_lvl = vine_config[['trunc_lvl']],
-    selcrit=vine_config[['selcrit']],
-    core=vine_config[['core']])
+    selcrit = vine_config[['selcrit']],
+    core = vine_config[['core']],
+    show_trace = vine_config[['show_trace']])
   print(Sys.time() - time_before)
-  return(vine_fit)
+  return(list(vine_fit=vine_fit, quantiles=xvine_data$quantiles, quantile_values=xvine_data$quantile_values))
 }
 
-ExtremeVineCollection <- function(dataset, uniform_dataset, horizons, vine_config){
+ExtremeVineCollection <- function(dataset, uniform_dataset, horizons, vine_config, rescaling=F){
   # Structure: colnames -> horizons
   n_col <- ncol(dataset)
   result_vines <- lapply(
@@ -72,7 +92,7 @@ ExtremeVineCollection <- function(dataset, uniform_dataset, horizons, vine_confi
     function(col_number){
         return(lapply(
           horizons,
-          function(horizon){return(ExtremeVineFit(dataset, uniform_dataset, col_number, horizon, vine_config))}
+          function(horizon){return(ExtremeVineFit(dataset, uniform_dataset, col_number, horizon, vine_config, rescaling))}
         )
       )
     }
@@ -175,118 +195,126 @@ ExtremeVineExtractConditional <- function(vine, level){
   }
 }
 
-ExtremeVineSingleCondSim <- function(vine, col_number, value){
+# handle when one computation has to be handle before the other
+ExtremeVineCondSimSingle <- function(vine, col_number, value){
   sim_vals <- rep(0, vine$structure$d)
   sim_vals[col_number] <- value
   random_start <- runif(vine$structure$d) # uncorrelated unif(0,1)
   
   for(level in 1:(vine$structure$trunc_lvl-1)){
-    already_simulated_vars <- which(sim_vals > 0.0)
-    
     links <- ExtremeVineExtractLink(vine, level)
+    already_simulated_vars <- which(sim_vals > 0.0)
     links_with_only_one <- apply(
       links,
       MARGIN = 1,
       function(link){sum(as.numeric(already_simulated_vars %in% link))==1})
     
-    which_to_choose <- which(links_with_only_one)
-    
-    links <- links[links_with_only_one,]
-    cond_vars <- NA
-    if(level > 1){
-      cond_vars <- ExtremeVineExtractConditional(vine, level = level)
-      is_link_usable_cond <- apply(cond_vars,
-                                   MARGIN = 1,
-                                   FUN = function(lk_cond_var){
-                                     all(lk_cond_var %in% already_simulated_vars)
-                                   })
-      cond_vars <- cond_vars[is_link_usable_cond,]
+    while(any(links_with_only_one)){
+      which_to_choose <- which(links_with_only_one)
+      
+      links <- links[links_with_only_one,]
+      cond_vars <- NA
+      if(level > 1){
+        cond_vars <- ExtremeVineExtractConditional(vine, level = level)
+        is_link_usable_cond <- apply(cond_vars,
+                                     MARGIN = 1,
+                                     FUN = function(lk_cond_var){
+                                       all(lk_cond_var %in% already_simulated_vars)
+                                     })
+        cond_vars <- cond_vars[is_link_usable_cond,]
+        
+        if(is.null(dim(links))){
+          links <- links[is_link_usable_cond[links_with_only_one]]
+        }else{
+          links <- links[is_link_usable_cond[links_with_only_one],]
+        }
+        which_to_choose <- which_to_choose[is_link_usable_cond]
+      }
+      target_vars <- apply(
+        as.matrix(links),
+        MARGIN = 1,
+        function(link){!(link %in% already_simulated_vars)})
+      target_vars <- t(target_vars)
+      already_simed_vars <- links[!target_vars]
+      target_vars <- links[target_vars]
       
       if(is.null(dim(links))){
-        links <- links[is_link_usable_cond[links_with_only_one]]
-      }else{
-        links <- links[is_link_usable_cond[links_with_only_one],]
+        links <- matrix(links, ncol=2)
       }
-      which_to_choose <- which_to_choose[is_link_usable_cond]
-    }
-    target_vars <- apply(
-      as.matrix(links),
-      MARGIN = 1,
-      function(link){!(link %in% already_simulated_vars)})
-    target_vars <- t(target_vars)
-    already_simed_vars <- links[!target_vars]
-    target_vars <- links[target_vars]
-    
-    if(is.null(dim(links))){
-      links <- matrix(links, ncol=2)
-    }
-    
-    if(all(vapply(which_to_choose, is.na, FUN.VALUE = NA))){break}
-    
-    tmp <- t(vapply(1:nrow(links),
-                    function(i){
-                      bicop_tmp <- vine$pair_copulas[[level]][[which_to_choose[i]]]
-                      bicop_data_with_cond <- ExtremeVinePlacingConditional(
-                        rdm_data = sim_vals[already_simed_vars[i]],
-                        cond_data = random_start[target_vars[i]],
-                        cond_on =  which(links[i,]==target_vars[i])) # first level of interaction, we use uniforms as per Bevacqua et al. (2017)
-                      val <- rvinecopulib::hbicop(u=bicop_data_with_cond,
-                                                  cond_var = which(links[i,]==target_vars[i]),
-                                                  bicop_tmp,
-                                                  inverse = F)
-                      
-                      # we know that cond_vars variables have been simulated
-                      if(level > 1){
-                        conditional_level <- level
-                        current_target <- target_vars[i]
-                        current_cond <- NA
-                        if(is.null(dim(cond_vars))){
-                          conditional_vals <- cond_vars
-                        }else{
-                          conditional_vals <- cond_vars[i,]
-                        }
+      
+      if(all(vapply(which_to_choose, is.na, FUN.VALUE = NA))){break}
+      tmp <- t(vapply(1:nrow(links),
+                      function(i){
+                        bicop_tmp <- vine$pair_copulas[[level]][[which_to_choose[i]]]
+                        bicop_data_with_cond <- ExtremeVinePlacingConditional(
+                          rdm_data = sim_vals[already_simed_vars[i]],
+                          cond_data = random_start[target_vars[i]],
+                          cond_on =  which(links[i,]==target_vars[i])) # first level of interaction, we use uniforms as per Bevacqua et al. (2017)
+                        val <- rvinecopulib::hbicop(u=bicop_data_with_cond,
+                                                    cond_var = which(links[i,]==target_vars[i]),
+                                                    bicop_tmp,
+                                                    inverse = T)
                         
-                        while(conditional_level > 1){
-                          more_than_one <- F
-                          for(cond_v in conditional_vals){
-                            if(!more_than_one){
-                              extracted_bicop <- ExtremeVineExtractBicop(
-                                vine, c(current_target, cond_v))
-                              current_cond <- cond_v
-                              # checking if the link does exist
-                              if(!is.na(extracted_bicop$level)){
-                                if(extracted_bicop$level+1 == conditional_level){
-                                  more_than_one <- TRUE
-                                  conditional_level <- extracted_bicop$level
-                                  
-                                  assertthat::assert_that(sim_vals[cond_v] > 0.0)
-                                  
-                                  val <- rvinecopulib::hbicop(u=c(val, sim_vals[cond_v]),
-                                                              cond_var = 2,
-                                                              extracted_bicop$bicop,
-                                                              inverse = F)
-                                }else{
-                                  # warning('extracted_bicop$level+1 != conditional_level')
+                        # we know that cond_vars variables have been simulated
+                        if(level > 1){
+                          conditional_level <- level
+                          current_target <- target_vars[i]
+                          current_cond <- NA
+                          if(is.null(dim(cond_vars))){
+                            conditional_vals <- cond_vars
+                          }else{
+                            conditional_vals <- cond_vars[i,]
+                          }
+                          
+                          while(conditional_level > 1){
+                            more_than_one <- F
+                            for(cond_v in conditional_vals){
+                              if(!more_than_one){
+                                extracted_bicop <- ExtremeVineExtractBicop(
+                                  vine, c(current_target, cond_v))
+                                current_cond <- cond_v
+                                # checking if the link does exist
+                                if(!is.na(extracted_bicop$level)){
+                                  if(extracted_bicop$level+1 == conditional_level){
+                                    more_than_one <- TRUE
+                                    conditional_level <- extracted_bicop$level
+                                    
+                                    assertthat::assert_that(sim_vals[cond_v] > 0.0)
+                                    val <- rvinecopulib::hbicop(u=c(val, sim_vals[cond_v]),
+                                                                cond_var = 2,
+                                                                extracted_bicop$bicop,
+                                                                inverse = T)
+                                  }else{
+                                    # warning('extracted_bicop$level+1 != conditional_level')
+                                  }
                                 }
                               }
                             }
+                            current_links <- ExtremeVineExtractLink(vine, conditional_level)
+                            conditional_vals <- ExtremeVineExtractConditional(vine, conditional_level)
+                            
+                            is_condi_link <- apply(current_links, 1,
+                                                   function(lk){all(c(current_target, current_cond) %in% lk)})
+                            if(is.null(dim(conditional_vals))){
+                              conditional_vals <- conditional_vals[is_condi_link]
+                            }else{
+                              conditional_vals <- conditional_vals[is_condi_link,]
+                            }
+                            
                           }
-                          current_links <- ExtremeVineExtractLink(vine, conditional_level)
-                          conditional_vals <- ExtremeVineExtractConditional(vine, conditional_level)
-                          
-                          is_condi_link <- apply(current_links, 1,
-                                                 function(lk){all(c(current_target, current_cond) %in% lk)})
-                          if(is.null(dim(conditional_vals))){
-                            conditional_vals <- conditional_vals[is_condi_link]
-                          }else{
-                            conditional_vals <- conditional_vals[is_condi_link,]
-                          }
-                          
                         }
-                      }
-                      return(c(target_vars[i], val))},
-                    c(1,2)))
-    sim_vals[tmp[,1]] <- tmp[,2]
+                        return(c(target_vars[i], val))},
+                      c(1,2)))
+      sim_vals[tmp[,1]] <- tmp[,2]
+      
+      # updating links_with_only_one which might have unlock the access to a variable
+      already_simulated_vars <- which(sim_vals > 0.0)
+      links <- ExtremeVineExtractLink(vine, level)
+      links_with_only_one <- apply(
+        links,
+        MARGIN = 1,
+        function(link){sum(as.numeric(already_simulated_vars %in% link))==1})
+    }
   }
   
   return(sim_vals)
@@ -296,7 +324,7 @@ ExtremeVineConditionalSimulation <- function(vine, col_number, value, n, seed=42
   set.seed(42)
   return(
     t(
-      vapply(1:n, FUN = function(i){ExtremeVineSingleCondSim(vine, col_number, value)}, rep(0, vine_tmp$structure$d))
+      vapply(1:n, FUN = function(i){ExtremeVineCondSimSingle(vine, col_number, value)}, rep(0, vine_tmp$structure$d))
       )
     )
 }
--- vine.R
+++ vine.R
@@ -19,9 +19,9 @@ unif_pollution_data <- UniformFromGPDForMatrix(
 vine_config <- list()
 vine_config[['family_set']] =  c("clayton", "gumbel", "indep")
 vine_config[['trunc_lvl']] = NA
-vine_config[['selcrit']] = 'mbicv'
+vine_config[['selcrit']] = 'aic'
 vine_config[['core']] = parallel::detectCores()-1
-
+vine_config[['show_trace']] = FALSE
 # full_vine_mbic <- rvinecopulib::vinecop(data = unif_pollution_data,
 #                                         family_set = c("clayton", "gumbel", "indep"),
 #                                         trunc_lvl = NA,
@@ -38,131 +38,30 @@ vine_config[['core']] = parallel::detectCores()-1
 #                                         core=parallel::detectCores()-1)
 # print(full_vine_bic)
 
-evc <- ExtremeVineCollection(pollution_data, unif_pollution_data, horizons = c(1,2), vine_config = vine_config)
-evc$O3[[1]]$structure
+evc <- ExtremeVineCollection(pollution_data, unif_pollution_data, horizons = c(1), vine_config = vine_config, rescaling = T)
+evc$O3[[1]]$quantile_values
 
-vine_tmp <- evc$O3[[1]]
-vine_struc <- evc$O3[[1]]$structure
-col_number_tmp <- 3
-sim_vals <- rep(0, vine_tmp$structure$d)
-sim_vals[col_number_tmp] <- 0.98
-random_start <- runif(vine_tmp$structure$d)
 
-for(level in 1:(vine_tmp$structure$trunc_lvl-1)){
-  already_simulated_vars <- which(sim_vals > 0.0)
-  
-  links <- ExtremeVineExtractLink(vine_tmp, level)
-  links_with_only_one <- apply(
-    links,
-    MARGIN = 1,
-    function(link){sum(as.numeric(already_simulated_vars %in% link))==1})
-  
-  which_to_choose <- which(links_with_only_one)
-  
-  links <- links[links_with_only_one,]
-  cond_vars <- NA
-  if(level > 1){
-    cond_vars <- ExtremeVineExtractConditional(vine_tmp, level = level)
-    is_link_usable_cond <- apply(cond_vars,
-          MARGIN = 1,
-          FUN = function(lk_cond_var){
-            all(lk_cond_var %in% already_simulated_vars)
-          })
-    cond_vars <- cond_vars[is_link_usable_cond,]
+col_number_tmp <- 1
+cond_value <- 0.96
+vine_tmp <- evc[[col_number_tmp]][[3]]$vine_fit
+vine_struc <- evc[[col_number_tmp]][[3]]$vine_fit$structure
 
-    if(is.null(dim(links))){
-      links <- links[is_link_usable_cond[links_with_only_one]]
-    }else{
-      links <- links[is_link_usable_cond[links_with_only_one],]
-    }
-    which_to_choose <- which_to_choose[is_link_usable_cond]
-  }
-  target_vars <- apply(
-    as.matrix(links),
-    MARGIN = 1,
-    function(link){!(link %in% already_simulated_vars)})
-  target_vars <- t(target_vars)
-  already_simed_vars <- links[!target_vars]
-  target_vars <- links[target_vars]
-  
-  if(is.null(dim(links))){
-    links <- matrix(links, ncol=2)
-  }
-  
-  if(all(vapply(which_to_choose, is.na, FUN.VALUE = NA))){break}
-  
-  tmp <- t(vapply(1:nrow(links),
-                function(i){
-    bicop_tmp <- vine_tmp$pair_copulas[[level]][[which_to_choose[i]]]
-    bicop_data_with_cond <- ExtremeVinePlacingConditional(
-      rdm_data = sim_vals[already_simed_vars[i]],
-      cond_data = random_start[target_vars[i]],
-      cond_on =  which(links[i,]==target_vars[i]))
-    val <- rvinecopulib::hbicop(u=bicop_data_with_cond,
-                                cond_var = which(links[i,]==target_vars[i]),
-                                bicop_tmp,
-                                inverse = F)
+samples <- t(vapply(950:1000/1000, function(x){ apply(ExtremeVineConditionalSimulation(vine_tmp, col_number = col_number_tmp, value = x, n = 1), 2, mean)},
+                    rep(0, ncol(pollution_data))))
 
-    # we know that cond_vars variables have been simulated
-    if(level > 1){
-      conditional_level <- level
-      current_target <- target_vars[i]
-      current_cond <- NA
-      if(is.null(dim(cond_vars))){
-        conditional_vals <- cond_vars
-      }else{
-        conditional_vals <- cond_vars[i,]
-      }
-      
-      
-      while(conditional_level > 1){
-        more_than_one <- F
-        for(cond_v in conditional_vals){
-          if(!more_than_one){
-            extracted_bicop <- ExtremeVineExtractBicop(
-              vine_tmp, c(current_target, cond_v))
-            current_cond <- cond_v
-            # checking if the link does exist
-            if(!is.na(extracted_bicop$level)){
-              if(extracted_bicop$level+1 == conditional_level){
-                more_than_one <- TRUE
-                conditional_level <- extracted_bicop$level
-                
-                assertthat::assert_that(sim_vals[cond_v] > 0.0)
-                
-                val <- rvinecopulib::hbicop(u=c(val, sim_vals[cond_v]),
-                                            cond_var = 2,
-                                            extracted_bicop$bicop,
-                                            inverse = F)
-              }else{
-                # warning('extracted_bicop$level+1 != conditional_level')
-              }
-            }
-          }
-        }
-        current_links <- ExtremeVineExtractLink(vine_tmp, conditional_level)
-        conditional_vals <- ExtremeVineExtractConditional(vine_tmp, conditional_level)
-
-        is_condi_link <- apply(current_links, 1,
-                            function(lk){all(c(current_target, current_cond) %in% lk)})
-        if(is.null(dim(conditional_vals))){
-          conditional_vals <- conditional_vals[is_condi_link]
-        }else{
-          conditional_vals <- conditional_vals[is_condi_link,]
-        }
-        
-      }
-    }
-    # val <- val[which(!simed_var_bicop[i,])]
-    return(c(target_vars[i], val))},
-    c(1,2)))
-  sim_vals[tmp[,1]] <- tmp[,2]
-  print('sim_vals')
-  print(sim_vals)
+par(mai=c(0.9,0.6,0.4,0.1), mfrow=c(6,1))
+plot(950:1000/1000, samples[,col_number_tmp],
+     ylab=paste('Probability of', colnames(pollution_data)[col_number_tmp]),
+     xlab = paste(colnames(pollution_data)[col_number_tmp], ' quantiles'), main = 'Probability of extremes',
+     cex.lab=1.5, cex.axis=1.4)
+for(i in c(1:ncol(pollution_data))[-col_number_tmp]){
+  plot(950:1000/1000, samples[,i],
+       ylab=paste('Probability of', colnames(pollution_data)[i]),
+       xlab=paste(colnames(pollution_data)[col_number_tmp], ' quantiles'),
+       cex.lab=1.5, cex.axis=1.4)
+  abline(h = evc[[col_number_tmp]][[1]]$quantile_values[i], lty=2, lwd=2)
 }
-  
-sim_vals
 
-# simulating
 
 ExtremeVineExtractConditional(evc$O3[[1]], 4)
